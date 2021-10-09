92dd87d32194b3491998b0c5dcac6d2441546c4b
Roman Tikhonov <ggys@yandex-team.ru>
2021-10-09 13:03:56 +0300

    doubly linked list impl

--- src/main/kotlin/DoublyLinkedList.kt
+++ src/main/kotlin/DoublyLinkedList.kt
@@ -1,16 +1,14 @@
-data class Node<K, V>(var next: Node<K, V>?, var prev: Node<K, V>?, val key: K?, var value: V?) : Iterator<Node<K, V>> {
+import java.lang.IllegalStateException
+import javax.swing.text.html.HTMLDocument
+
+data class Node<K, V>(var next: Node<K, V>?, var prev: Node<K, V>?, val key: K?, var value: V?) {
     constructor() : this(null, null, null, null)
-
-    override fun hasNext() = next != null
-
-    override fun next() = next ?: throw NoSuchElementException()
+    constructor(key: K?, value: V?) : this(null, null, key, value)
 }
 
 class DoublyLinkedList<K, V> : Iterable<Node<K, V>> {
     private val head = Node<K, V>()
-    private val tail = head
-    var size = 0
-        private set
+    private var tail = head
 
     val first: Node<K, V>
         get() = head.next ?: throw NoSuchElementException()
@@ -19,16 +17,48 @@ class DoublyLinkedList<K, V> : Iterable<Node<K, V>> {
         get() = if (head != tail) tail else throw NoSuchElementException()
 
     fun moveToFront(node: Node<K, V>) {
-        TODO("Not yet implemented")
+        if (node == tail) {
+            tail = node.prev ?: throw IllegalStateException("Must be pre-tail node when non empty.")
+        }
+        node.apply {
+            next?.prev = prev
+            prev?.next = next
+            next = null
+            prev = null
+            addFirst(this)
+        }
     }
 
     fun removeLast() {
-        TODO("Not yet implemented")
+        tail.prev?.let { prev ->
+            prev.next = null
+            tail.prev = null
+            tail = prev
+        }
     }
 
     fun addFirst(key: K, value: V): Node<K, V> {
-        TODO("Not yet implemented")
+        return addFirst(Node(key, value))
     }
 
-    override fun iterator() = head
+    private fun addFirst(newNode: Node<K, V>): Node<K, V> {
+        return newNode.apply {
+            next = head.next
+            prev = head
+            next?.prev = this
+            head.next = this
+            if (head == tail) {
+                tail = this
+            }
+        }
+    }
+
+    override fun iterator() = object : Iterator<Node<K, V>> {
+        private var iterator: Node<K, V> = head
+        override fun hasNext() = iterator.next != null
+        override fun next(): Node<K, V> {
+            iterator = iterator.next ?: throw NoSuchElementException()
+            return iterator
+        }
+    }
 }
